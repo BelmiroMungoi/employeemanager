36b93db2466ae2362e43dd353c2f7f2e772ee31d
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-26 17:25:32 +0200

    adicionando o repository para a persistencia de dados

---
+++ src/main/java/com/bbm/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.bbm.employeemanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bbm.employeemanager.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
