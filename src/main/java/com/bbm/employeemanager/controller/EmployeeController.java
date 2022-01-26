bcff0bbef200fd7ca72e543218f979d88957e602
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-26 20:27:16 +0200

    creating the controller for the api

---
+++ src/main/java/com/bbm/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.bbm.employeemanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+}
