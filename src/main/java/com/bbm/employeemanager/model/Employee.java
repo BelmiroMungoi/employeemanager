3add63d312bcd59877c8dd8fe42a3b13669809a0
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-26 15:59:36 +0200

    creating the Employee entity

---
+++ src/main/java/com/bbm/employeemanager/model/Employee.java
@@ -0,0 +1,102 @@
+package com.bbm.employeemanager.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phone;
+	private String image;
+	
+	@Column(nullable = false, updatable = false)
+	private String employeCode;
+	
+	public Employee() {
+	}
+	
+	public Employee(Long id, String name, String email, String jobTitle, String phone, String image,
+			String employeCode) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.jobTitle = jobTitle;
+		this.phone = phone;
+		this.image = image;
+		this.employeCode = employeCode;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getEmployeCode() {
+		return employeCode;
+	}
+
+	public void setEmployeCode(String employeCode) {
+		this.employeCode = employeCode;
+	}
+
+}
