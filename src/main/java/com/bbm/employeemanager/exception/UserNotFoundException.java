c602d972ae71266ef9be11123e7eadb6e8be52d2
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-26 20:26:58 +0200

    creating an exception

---
+++ src/main/java/com/bbm/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.bbm.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+}
