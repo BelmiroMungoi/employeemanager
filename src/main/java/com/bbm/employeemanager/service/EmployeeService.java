6ef45afd57903cc7b88b8ffdbe320b73c5ef0972
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-26 20:26:44 +0200

    creating persistence methods

--- src/main/java/com/bbm/employeemanager/service/EmployeeService.java
+++ src/main/java/com/bbm/employeemanager/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.bbm.employeemanager.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bbm.employeemanager.exception.UserNotFoundException;
 import com.bbm.employeemanager.model.Employee;
 import com.bbm.employeemanager.repository.EmployeeRepository;
 
@@ -13,9 +15,26 @@ public class EmployeeService {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	public Employee addEmployee(Employee employee) {
 		employee.setEmployeCode(UUID.randomUUID().toString());
 		return employeeRepository.save(employee);
+	}
+
+	public List<Employee> findAllEmployee() {
+		return employeeRepository.findAll();
+	}
+
+	public Employee findEmployeeById(Long id) {
+		return employeeRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException("Usuário com o ID " + id + " não foi encontrado!"));
+	}
+
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	public void deleteEmployee(Long id) {
+		employeeRepository.deleteById(id);
 	}
 }
