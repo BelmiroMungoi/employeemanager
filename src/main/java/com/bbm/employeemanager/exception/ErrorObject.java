7ca08e87b233f2f149798883972151f3dd7c40b2
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-27 12:01:00 +0200

    model for mapping errors

---
+++ src/main/java/com/bbm/employeemanager/exception/ErrorObject.java
@@ -0,0 +1,78 @@
+package com.bbm.employeemanager.exception;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class ErrorObject {
+
+	private String status;
+	private OffsetDateTime time;
+	private String title;
+	private List<Campo> campos;
+
+	public static class Campo {
+
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+
+		private String nome;
+		private String mensagem;
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getMensagem() {
+			return mensagem;
+		}
+
+		public void setMensagem(String mensagem) {
+			this.mensagem = mensagem;
+		}
+
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public OffsetDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(OffsetDateTime time) {
+		this.time = time;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Campo> getCampos() {
+		return campos;
+	}
+
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+
+}
