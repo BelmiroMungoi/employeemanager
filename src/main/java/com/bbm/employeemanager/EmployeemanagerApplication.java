9470e03250229c80ccdb4f0623cf8af1d9e95293
Belmiro Mungoi <belmiromungoi@gmail.com>
2022-01-31 10:19:55 +0200

    adding a CORS configuration

--- src/main/java/com/bbm/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/bbm/employeemanager/EmployeemanagerApplication.java
@@ -2,12 +2,24 @@ package com.bbm.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class EmployeemanagerApplication {
+@ComponentScan(basePackages = "com.*")
+public class EmployeemanagerApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// Libera o acesso aos End-Points da API
+		registry.addMapping("/**")
+		.allowedMethods("*")// libera quais metodos https(GET, POST, etc) podem ser acessados
+				.allowedOrigins("*");// libera o acesso para requisicoes de outros servers
 	}
 
 }
